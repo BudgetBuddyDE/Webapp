b94eaebe17ccd1ec31527db21c085db88adea85f
Thorben <38816229+tklein1801@users.noreply.github.com>
2022-08-02 19:37:10 +0200

    feat: Add search-input component

---
+++ src/components/search-input.component.tsx
@@ -0,0 +1,67 @@
+import React, { FC } from 'react';
+import debounce from 'lodash.debounce';
+import { styled, alpha, SxProps, Theme } from '@mui/material/styles';
+import InputBase from '@mui/material/InputBase';
+import { Search as SearchIcon } from '@mui/icons-material';
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(1),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '20ch',
+      '&:focus': {
+        width: '30ch',
+      },
+    },
+  },
+}));
+
+export interface SearchInputProps {
+  placeholder?: string;
+  onSearch: (text: string) => void;
+  sx?: SxProps<Theme>;
+}
+
+export const SearchInput: FC<SearchInputProps> = ({ placeholder = 'Search…', onSearch, sx }) => {
+  return (
+    <Search sx={sx}>
+      <SearchIconWrapper>
+        <SearchIcon />
+      </SearchIconWrapper>
+      <StyledInputBase
+        placeholder={placeholder}
+        inputProps={{ 'aria-label': 'search' }}
+        onChange={debounce((e) => onSearch(e.target.value), 150)}
+      />
+    </Search>
+  );
+};
