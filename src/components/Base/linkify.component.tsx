f28003b2d2350febd913a189fb3e996423ae8585
Thorben <38816229+tklein1801@users.noreply.github.com>
2023-02-19 14:42:32 +0100

    refactor: Add unique key for items

--- src/components/Base/linkify.component.tsx
+++ src/components/Base/linkify.component.tsx
@@ -5,19 +5,19 @@ const URL_REGEX =
   /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-zA-Z0-9]+([\-\.]{1}[a-zA-Z0-9]+)*\.[a-zA-Z]{2,5}(:[0-9]{1,5})?(\/.*)?$/gm;
 
 export const Linkify: React.FC<React.PropsWithChildren> = ({ children }) => {
+  const id = React.useId();
   if (typeof children !== 'string') {
     throw new Error('Provided children should be a String');
   }
-
   return (
     <Typography>
-      {children.split(' ').map((word) => {
+      {children.split(' ').map((word, index) => {
         return word.match(URL_REGEX) ? (
-          <>
+          <React.Fragment key={id + index}>
             <Link href={word.includes('//') ? word : '//' + word} underline="none">
               {word}
             </Link>{' '}
-          </>
+          </React.Fragment>
         ) : (
           word + ' '
         );
