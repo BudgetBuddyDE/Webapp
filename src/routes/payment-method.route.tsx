65665e5e1e0b5caed33d8614aeccb9dbe1eb3a82
Thorben <38816229+tklein1801@users.noreply.github.com>
2023-03-04 02:22:48 +0100

    feat: Create payment-method from option

--- src/routes/payment-method.route.tsx
+++ src/routes/payment-method.route.tsx
@@ -14,6 +14,7 @@ import {
   Typography,
 } from '@mui/material';
 import React from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 import {
   ActionPaper,
   Card,
@@ -36,6 +37,7 @@ import { PaymentMethod } from '../models';
 import { TablePaginationReducer } from '../reducer';
 
 interface PaymentMethodHandler {
+  clearLocatioState: () => void;
   onSearch: (keyword: string) => void;
   pagination: TablePaginationHandler;
   paymentMethod: {
@@ -44,18 +46,25 @@ interface PaymentMethodHandler {
 }
 
 export const PaymentMethods = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
   const { showSnackbar } = React.useContext(SnackbarContext);
   const { setPaymentMethods } = React.useContext(StoreContext);
   const fetchTransactions = useFetchTransactions();
   const fetchSubscriptions = useFetchSubscriptions();
   const fetchPaymentMethods = useFetchPaymentMethods();
   const [, startTransition] = React.useTransition();
-  const [showAddForm, setShowAddForm] = React.useState(false);
+  const [showAddForm, setShowAddForm] = React.useState(
+    location.state !== null && (location.state as any).create !== undefined && (location.state as any).create === true
+  );
   const [keyword, setKeyword] = React.useState('');
   const [editPaymentMethod, setEditPaymentMethod] = React.useState<PaymentMethod | null>(null);
   const [tablePagination, setTablePagination] = React.useReducer(TablePaginationReducer, InitialTablePaginationState);
 
   const handler: PaymentMethodHandler = {
+    clearLocatioState() {
+      window.history.replaceState(null, '');
+    },
     onSearch(keyword) {
       setKeyword(keyword.toLowerCase());
     },
@@ -98,6 +107,12 @@ export const PaymentMethods = () => {
     const { page, rowsPerPage } = tablePagination;
     return shownPaymentMethods.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage);
   }, [shownPaymentMethods, tablePagination]);
+
+  React.useEffect(() => {
+    return () => handler.clearLocatioState();
+  }, []);
+
+  React.useEffect(() => console.log(location), [location]);
 
   return (
     <Grid container spacing={3}>
@@ -214,7 +229,29 @@ export const PaymentMethods = () => {
         </Grid>
       </Grid>
 
-      <CreatePaymentMethod open={showAddForm} setOpen={(show) => setShowAddForm(show)} />
+      <CreatePaymentMethod
+        open={showAddForm}
+        setOpen={(show) => {
+          // If an location.state is set and used to create an payment-method
+          // we're gonna clear that state after it got closed (in order to remove the default paymentMethod)
+          if (
+            !show &&
+            location.state !== null &&
+            (location.state as any).create === true &&
+            (location.state as any).category !== undefined
+          ) {
+            navigate(location.pathname, { replace: true, state: null });
+          }
+          setShowAddForm(show);
+        }}
+        paymentMethod={
+          location.state &&
+          (location.state as any).create !== undefined &&
+          (location.state as any).paymentMethod !== undefined
+            ? (location.state as any).paymentMethod
+            : undefined
+        }
+      />
 
       <EditPaymentMethod
         open={editPaymentMethod !== null}
