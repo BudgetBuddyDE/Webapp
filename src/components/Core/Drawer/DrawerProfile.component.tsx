cb53427b81a0e30553ee1597e50fa0b5882c01e9
Thorben <38816229+tklein1801@users.noreply.github.com>
2023-07-07 11:43:19 +0200

    fix: Close drawer on profile-click
    Close drawer on mobile devices when clicking on your profile

--- src/components/Core/Drawer/DrawerProfile.component.tsx
+++ src/components/Core/Drawer/DrawerProfile.component.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { ProfileAvatar } from '@/components/Profile/ProfileAvatar.component';
 import { AuthContext } from '@/context/Auth.context';
+import { StoreContext } from '@/context/Store.context';
+import { useWindowDimensions } from '@/hook/useWindowDimensions.hook';
 import { SupabaseClient } from '@/supabase';
 import { Logout as LogoutIcon } from '@mui/icons-material';
 import { Box, Button, ButtonProps, Chip, Divider, Typography } from '@mui/material';
@@ -12,10 +14,19 @@ export type DrawerProfileProps = {
 
 export const DrawerProfile: React.FC<DrawerProfileProps> = ({ open }) => {
     const navigate = useNavigate();
+    const { breakpoint } = useWindowDimensions();
     const { session } = React.useContext(AuthContext);
+    const { setShowDrawer, showDrawer } = React.useContext(StoreContext);
 
     const handleSignOut = async () => {
         await SupabaseClient().auth.signOut();
+    };
+
+    const handleClick = () => {
+        navigate('/settings/profile');
+        if (showDrawer && (breakpoint == 'sm' || breakpoint == 'xs')) {
+            setShowDrawer(false);
+        }
     };
 
     return (
@@ -43,7 +54,7 @@ export const DrawerProfile: React.FC<DrawerProfileProps> = ({ open }) => {
                             cursor: 'Pointer',
                         },
                     }}
-                    onClick={() => navigate('/settings/profile')}
+                    onClick={handleClick}
                 >
                     {session && session.user && <ProfileAvatar />}
                     <Box sx={{ ml: '.5rem' }}>
