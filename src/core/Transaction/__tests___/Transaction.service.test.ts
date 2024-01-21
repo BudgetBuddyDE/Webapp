700efded23118cb8328450ab1f8f41bbb2093ee2
Thorben <38816229+tklein1801@users.noreply.github.com>
2024-01-21 14:50:59 +0100

    feat(#82): Sort receiver-options for autocomplete

---
+++ src/core/Transaction/__tests___/Transaction.service.test.ts
@@ -0,0 +1,46 @@
+import { subDays } from 'date-fns';
+import { TransactionService } from '../Transaction.service';
+import { TTransaction } from '@budgetbuddyde/types';
+
+describe('getUniqueReceivers', () => {
+  it('should return an empty array if transactions is empty', () => {
+    const transactions: TTransaction[] = [];
+    const result = TransactionService.getUniqueReceivers(transactions);
+    expect(result).toEqual([]);
+  });
+
+  it('should return an array of unique receivers', () => {
+    const transactions = [
+      { receiver: 'John', processedAt: new Date() },
+      { receiver: 'Jane', processedAt: new Date() },
+      { receiver: 'John', processedAt: new Date() },
+      { receiver: 'Alice', processedAt: new Date() },
+    ] as TTransaction[];
+    const result = TransactionService.getUniqueReceivers(transactions);
+    expect(result).toEqual(['John', 'Jane', 'Alice']);
+  });
+
+  it('should return an array of unique receivers within the specified days', () => {
+    const transactions = [
+      { receiver: 'John', processedAt: subDays(new Date(), 10) },
+      { receiver: 'Jane', processedAt: subDays(new Date(), 20) },
+      { receiver: 'John', processedAt: subDays(new Date(), 5) },
+      { receiver: 'Alice', processedAt: subDays(new Date(), 15) },
+    ] as TTransaction[];
+    const result = TransactionService.getUniqueReceivers(transactions, 15);
+    expect(result).toEqual(['John', 'Alice', 'Jane']);
+  });
+
+  it('should return an array of unique receivers sorted by frequency', () => {
+    const transactions = [
+      { receiver: 'John', processedAt: new Date() },
+      { receiver: 'Jane', processedAt: new Date() },
+      { receiver: 'John', processedAt: new Date() },
+      { receiver: 'Alice', processedAt: new Date() },
+      { receiver: 'Alice', processedAt: new Date() },
+      { receiver: 'Alice', processedAt: new Date() },
+    ] as TTransaction[];
+    const result = TransactionService.getUniqueReceivers(transactions);
+    expect(result).toEqual(['Alice', 'John', 'Jane']);
+  });
+});
