b9abbbbf255a23bcf98a010dbdbea5890e34a91c
Thorben <38816229+tklein1801@users.noreply.github.com>
2024-04-07 14:32:10 +0200

    refactor: Remove some components

--- src/routes/Dashboard/Budget.view.tsx
+++ src/routes/Dashboard/Budget.view.tsx
@@ -1,21 +1,11 @@
 import React from 'react';
-import {BudgetList, BudgetProgressWrapper, StatsWrapper, useFetchBudgetProgress} from '@/components/Budget';
+import {BudgetList, StatsWrapper} from '@/components/Budget';
 import {CategorySpendingsChart, CategoryIncomeChart} from '@/components/Category';
 import {Grid} from '@mui/material';
 import {DailyTransactionChart} from '@/components/Transaction';
-import {CircularProgress} from '@/components/Loading';
 import {MonthlyBalanceChartCard, MonthlyBalanceWidget} from '@/components/Transaction/MonthlyBalance';
 
-export const DATE_RANGE_INPUT_FORMAT = 'dd.MM';
-export type TChartContentType = 'INCOME' | 'SPENDINGS';
-export const ChartContentTypes = [
-  {type: 'INCOME' as TChartContentType, label: 'Income'},
-  {type: 'SPENDINGS' as TChartContentType, label: 'Spendings'},
-];
-
 export const BudgetView = () => {
-  const {budgetProgress, loading: loadingBudgetProgress} = useFetchBudgetProgress();
-
   return (
     <React.Fragment>
       <Grid item xs={12} md={12} lg={5} xl={5}>
@@ -31,12 +21,6 @@ export const BudgetView = () => {
 
         <Grid item xs={12} md={12} lg={12} xl={12}>
           <BudgetList />
-
-          {loadingBudgetProgress ? (
-            <CircularProgress />
-          ) : (
-            <BudgetProgressWrapper data={budgetProgress} cardProps={{sx: {mt: 2}}} />
-          )}
         </Grid>
 
         <Grid item xs={12} md={12} lg={6} xl={6}>
