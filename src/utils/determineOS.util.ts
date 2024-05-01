3450d81011ea230684a64fd0506d2c47af73f713
Thorben <38816229+tklein1801@users.noreply.github.com>
2024-05-01 14:58:09 +0200

    feat: Provide function to retrieve the current device os

---
+++ src/utils/determineOS.util.ts
@@ -0,0 +1,28 @@
+/**
+ * Represents the possible operating systems.
+ * @typedef {'Windows' | 'MacOS' | 'Linux' | 'Android' | 'iOS' | 'unknown'} TOS
+ */
+export type TOS = 'Windows' | 'MacOS' | 'Linux' | 'Android' | 'iOS' | 'unknown';
+
+/**
+ * Determines the operating system based on the user agent string.
+ * @param userAgent - The user agent string. Defaults to `window.navigator.userAgent`.
+ * @returns The operating system as a string.
+ */
+export function determineOS(userAgent = window.navigator.userAgent) : TOS {
+  let os: TOS = 'unknown';
+  
+  if (userAgent.indexOf('Win') !== -1) {
+    os = 'Windows';
+  } else if (userAgent.indexOf('Android') !== -1) {
+    os = 'Android';
+  } else if (userAgent.indexOf('iOS') !== -1 || userAgent.indexOf('iPhone' || userAgent.indexOf('iPad') !== -1) !== -1){
+    os = 'iOS';
+  } else if (userAgent.indexOf('Mac') !== -1) {
+    os = 'MacOS';
+  } else if (userAgent.indexOf('Linux') !== -1) {
+    os = 'Linux';
+  }
+
+  return os;
+};
