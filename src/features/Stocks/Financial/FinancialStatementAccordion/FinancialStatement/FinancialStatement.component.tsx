b4ab481c4f4990b06b6ca517754ab9e8e9ba24af
Thorben <38816229+tklein1801@users.noreply.github.com>
2024-11-25 23:53:13 +0100

    feat: Provide financial information in table-format

--- src/features/Stocks/Financial/FinancialStatementAccordion/FinancialStatement/FinancialStatement.component.tsx
+++ src/features/Stocks/Financial/FinancialStatementAccordion/FinancialStatement/FinancialStatement.component.tsx
@@ -1,5 +1,20 @@
 import {ExpandMoreRounded} from '@mui/icons-material';
-import {Accordion, AccordionDetails, AccordionSummary, Box, Tab, Tabs, Typography} from '@mui/material';
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Box,
+  Divider,
+  Tab,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Tabs,
+  Typography,
+} from '@mui/material';
 import React from 'react';
 
 import {BarChart, type TBarChartProps} from '@/components/Base/Charts';
@@ -21,14 +36,37 @@ export const FinancialStatement: React.FC<TFinancialStatementProps> = ({
 }) => {
   const [timeframe, setTimeframe] = React.useState(EFinancialStatementTimeframe.Yearly);
 
+  const dateFormatter = React.useCallback(
+    (date: Date): string => {
+      return timeframe === EFinancialStatementTimeframe.Yearly
+        ? date.getFullYear().toString()
+        : `${Formatter.formatDate().shortMonthName(date)} ${date.getFullYear()}`;
+    },
+    [timeframe],
+  );
   const currency = React.useMemo(() => stockDetails?.details.securityDetails?.currency, [stockDetails]);
   const chartData = React.useMemo(() => {
     return (
       stockDetails?.details.securityDetails?.[
         timeframe === EFinancialStatementTimeframe.Yearly ? 'annualFinancials' : 'quarterlyFinancials'
-      ].reverse() || []
+      ] || []
     );
   }, [stockDetails, timeframe]);
+  const tableData: {label: string; prevValue: number; latestValue: number}[] = React.useMemo(() => {
+    if (chartData.length < 2) return [];
+    const data = chartData.slice(0, 2);
+    return [
+      {label: 'Revenue', prevValue: data[1].revenue, latestValue: data[0].revenue},
+      {label: 'Gross Profit', prevValue: data[1].grossProfit, latestValue: data[0].grossProfit},
+      {label: 'EBITDA', prevValue: data[1].ebitda, latestValue: data[0].ebitda},
+      {label: 'Net Profit', prevValue: data[1].netIncome, latestValue: data[0].netIncome},
+    ];
+  }, [chartData, timeframe, dateFormatter]);
+  const tableLabels: string[] = React.useMemo(() => {
+    if (chartData.length < 2) return [];
+    const data = chartData.slice(0, 2);
+    return data.map(({date}) => dateFormatter(new Date(date)));
+  }, [tableData, timeframe]);
 
   if (chartData.length === 0) return null;
   return (
@@ -51,37 +89,63 @@ export const FinancialStatement: React.FC<TFinancialStatementProps> = ({
             xAxis={[
               {
                 scaleType: 'band',
-                data: chartData.map(({date}) =>
-                  timeframe === EFinancialStatementTimeframe.Yearly
-                    ? date.getFullYear()
-                    : `${Formatter.formatDate().shortMonthName(date)} ${date.getFullYear()}`,
-                ),
+                data: chartData.map(({date}) => dateFormatter(date)).reverse(),
                 valueFormatter: value => value.toString(),
               },
             ]}
             series={[
               {
                 label: `Revenue (${currency})`,
-                data: chartData.map(({revenue}) => revenue),
+                data: chartData.map(({revenue}) => revenue).reverse(),
                 valueFormatter: seriesFormatter,
               },
               {
                 label: `Gross Profit (${currency})`,
-                data: chartData.map(({grossProfit}) => grossProfit),
+                data: chartData.map(({grossProfit}) => grossProfit).reverse(),
                 valueFormatter: seriesFormatter,
               },
               {
                 label: `EBITDA (${currency})`,
-                data: chartData.map(({ebitda}) => ebitda),
+                data: chartData.map(({ebitda}) => ebitda).reverse(),
                 valueFormatter: seriesFormatter,
               },
               {
                 label: `Net Profit (${currency})`,
-                data: chartData.map(({netIncome}) => netIncome),
+                data: chartData.map(({netIncome}) => netIncome).reverse(),
                 valueFormatter: seriesFormatter,
               },
             ]}
           />
+        </Box>
+        <Box>
+          <Divider sx={{mt: 2}} />
+          <TableContainer>
+            <Table size="small">
+              <TableHead>
+                <TableRow>
+                  <TableCell>({currency})</TableCell>
+                  {tableLabels.reverse().map(label => (
+                    <TableCell align="right" key={label}>
+                      {label}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {tableData.map(row => (
+                  <TableRow
+                    key={row.label.toLowerCase().replaceAll(/ /g, '_')}
+                    sx={{'&:last-child td, &:last-child th': {border: 0}}}>
+                    <TableCell component="th" scope="row">
+                      {row.label}
+                    </TableCell>
+                    <TableCell align="right">{Formatter.shortenNumber(row.prevValue)}</TableCell>
+                    <TableCell align="right">{Formatter.shortenNumber(row.latestValue)}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
         </Box>
       </AccordionDetails>
     </Accordion>
--- src/features/Stocks/Financial/FinancialStatementAccordion/ProfitLoss/ProfitLoss.component.tsx
+++ src/features/Stocks/Financial/FinancialStatementAccordion/ProfitLoss/ProfitLoss.component.tsx
@@ -1,5 +1,20 @@
 import {ExpandMoreRounded} from '@mui/icons-material';
-import {Accordion, AccordionDetails, AccordionSummary, Box, Tab, Tabs, Typography} from '@mui/material';
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Box,
+  Divider,
+  Tab,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Tabs,
+  Typography,
+} from '@mui/material';
 import React from 'react';
 
 import {BarChart, type TBarChartProps} from '@/components/Base/Charts';
@@ -17,14 +32,35 @@ export type TProfitLossProps = {
 export const ProfitLoss: React.FC<TProfitLossProps> = ({barChartProps, stockDetails, seriesFormatter}) => {
   const [timeframe, setTimeframe] = React.useState(EFinancialStatementTimeframe.Yearly);
 
+  const dateFormatter = React.useCallback(
+    (date: Date): string => {
+      return timeframe === EFinancialStatementTimeframe.Yearly
+        ? date.getFullYear().toString()
+        : `${Formatter.formatDate().shortMonthName(date)} ${date.getFullYear()}`;
+    },
+    [timeframe],
+  );
   const currency = React.useMemo(() => stockDetails?.details.securityDetails?.currency, [stockDetails]);
   const chartData = React.useMemo(() => {
     return (
       stockDetails?.details.securityDetails?.[
         timeframe === EFinancialStatementTimeframe.Yearly ? 'annualFinancials' : 'quarterlyFinancials'
-      ].reverse() || []
+      ] || []
     );
   }, [stockDetails, timeframe]);
+  const tableData: {label: string; prevValue: number; latestValue: number}[] = React.useMemo(() => {
+    if (chartData.length < 2) return [];
+    const data = chartData.slice(0, 2);
+    return [
+      {label: 'Revenue', prevValue: data[1].revenue, latestValue: data[0].revenue},
+      {label: 'Net Profit', prevValue: data[1].netIncome, latestValue: data[0].netIncome},
+    ];
+  }, [chartData, timeframe, dateFormatter]);
+  const tableLabels: string[] = React.useMemo(() => {
+    if (chartData.length < 2) return [];
+    const data = chartData.slice(0, 2);
+    return data.map(({date}) => dateFormatter(new Date(date)));
+  }, [tableData, timeframe]);
 
   if (chartData.length === 0) return null;
   return (
@@ -47,27 +83,53 @@ export const ProfitLoss: React.FC<TProfitLossProps> = ({barChartProps, stockDeta
             xAxis={[
               {
                 scaleType: 'band',
-                data: chartData.map(({date}) =>
-                  timeframe === EFinancialStatementTimeframe.Yearly
-                    ? date.getFullYear()
-                    : `${Formatter.formatDate().shortMonthName(date)} ${date.getFullYear()}`,
-                ),
+                data: chartData.map(({date}) => dateFormatter(date)).reverse(),
                 valueFormatter: value => value.toString(),
               },
             ]}
             series={[
               {
                 label: `Revenue (${currency})`,
-                data: chartData.map(({revenue}) => revenue),
+                data: chartData.map(({revenue}) => revenue).reverse(),
                 valueFormatter: seriesFormatter,
               },
               {
                 label: `Net Profit (${currency})`,
-                data: chartData.map(({netIncome}) => netIncome),
+                data: chartData.map(({netIncome}) => netIncome).reverse(),
                 valueFormatter: seriesFormatter,
               },
             ]}
           />
+        </Box>
+        <Box>
+          <Divider sx={{mt: 2}} />
+          <TableContainer>
+            <Table size="small">
+              <TableHead>
+                <TableRow>
+                  <TableCell>({currency})</TableCell>
+                  {tableLabels.reverse().map(label => (
+                    <TableCell align="right" key={label}>
+                      {label}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {tableData.map(row => (
+                  <TableRow
+                    key={row.label.toLowerCase().replaceAll(/ /g, '_')}
+                    sx={{'&:last-child td, &:last-child th': {border: 0}}}>
+                    <TableCell component="th" scope="row">
+                      {row.label}
+                    </TableCell>
+                    <TableCell align="right">{Formatter.shortenNumber(row.prevValue)}</TableCell>
+                    <TableCell align="right">{Formatter.shortenNumber(row.latestValue)}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
         </Box>
       </AccordionDetails>
     </Accordion>
